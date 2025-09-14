597d282661cb02723a816a19ff1db5d2c32409eb
9c5s <98406118+9c5s@users.noreply.github.com>
2025-09-14 22:57:25 +0900

    PowerShell設定更新: oh-my-poshテーマをemodipt-extendに設定

--- win/pwsh/Microsoft.PowerShell_profile.ps1
+++ win/pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
 function ll { eza -lahF --time-style "+%y/%m/%d %H:%M" @args }
 function claude { & "~/.local/bin/claude.exe" --dangerously-skip-permissions @args }
-oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/emodipt-extend.omp.json' | Invoke-Expression
