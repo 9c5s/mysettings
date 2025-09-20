6ef2a7d9acfb695d00510ea77f329ee52e1fa450
9c5s <98406118+9c5s@users.noreply.github.com>
2025-09-20 23:46:56 +0900

    $PROFILE更新: uv/uvx自動補完追加

--- win/pwsh/Microsoft.PowerShell_profile.ps1
+++ win/pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,10 @@
+# コマンドエイリアス
 function ll { eza -lahF --time-style "+%y/%m/%d %H:%M" @args }
 function claude { & "~/.local/bin/claude.exe" --dangerously-skip-permissions @args }
+
+# oh-my-posh設定
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/emodipt-extend.omp.json' | Invoke-Expression
+
+# uv/uvx自動補完
+(& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
+(& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
