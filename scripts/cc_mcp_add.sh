11c306c3355d21bc711bfc55f3fe4f6887b10575
9c5s <98406118+9c5s@users.noreply.github.com>
2026-02-16 18:37:31 +0900

    追加サーバー更新

--- scripts/cc_mcp_add.sh
+++ scripts/cc_mcp_add.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Claude MCP (Model Context Protocol) に各種サーバーを追加する設定スクリプト
-claude mcp add --transport http github-server https://api.githubcopilot.com/mcp -H "Authorization: Bearer ${GITHUB_PAT}"
-claude mcp add --transport http context7 https://mcp.context7.com/mcp
-claude mcp add --transport sse deepwiki https://mcp.deepwiki.com/sse
-claude mcp add playwright npx @playwright/mcp@latest
+# ClaudeCodeにに各種MCPサーバーを追加する設定スクリプト
+claude mcp add -s user -t http context7 https://mcp.context7.com/mcp --header "CONTEXT7_API_KEY: ctx7sk-8ad094ff-635c-4423-963b-6809a544029b"
+claude mcp add -s user -t http deepwiki https://mcp.deepwiki.com/mcp
+claude mcp add -s user -t stdio playwright -- bunx @playwright/mcp@latest
+claude mcp add -s user -t stdio chrome-devtools -- bunx chrome-devtools-mcp@latest
+claude mcp add -s user -t stdio drawio -- bunx @drawio/mcp
